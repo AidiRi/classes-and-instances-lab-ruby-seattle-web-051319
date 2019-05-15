f2db55bef63e55b21c32996252de1e9591b4dcef
AidiRi <rivera.aidi@gmail.com>
2019-05-15 09:39:22 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+# 3. Define a Person in lib/person.rb
+# Open lib/person.rb and add a class definition for a Person
+# class.
+class Person
+end
+# 4. Make 2 people in lib/person.rb
+# Under your Person class definition, create two people in
+# local variables, adele_goldberg and alan_kay
+adele_goldberg = Person.new
+alan_kay = Person.new
+# When you're done, submit the lab with learn submit.
